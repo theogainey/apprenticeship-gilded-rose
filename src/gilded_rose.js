99a52f1da526098629c454b82130b305e7d32cbf
Rise Erpelding <rise@heysparkbox.com>
2021-02-17 08:55:05 -0500

    refactor: move sulfuras logic
    Since there are several nested if statements specifying action to take
    if item name is NOT Sulfuras, this simplifies the code by removing all
    statements stating `if (items[i].name != 'Sulfuras, Hand of Ragnaros')`
    and instead creating a case for `if (items[i].name === 'Sulfuras...')`
    at the top. Used `break` because Sulfuras items do not change in sell_in
    or quality, though perhaps this could be changed later.
    
    Note that none of the statements under each `if (items[i].name !=
    'Sulfuras, Hand of Ragnaros')` were removed, just the if statements
    themselves.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -23,11 +23,12 @@ updateQuality(items);
 */
 export function updateQuality(items) {
   for (var i = 0; i < items.length; i++) {
+    if (items[i].name === 'Sulfuras, Hand of Ragnaros') {
+      break;
+    }
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
+        items[i].quality = items[i].quality - 1
       }
     } else {
       if (items[i].quality < 50) {
@@ -46,16 +47,14 @@ export function updateQuality(items) {
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
+
+    items[i].sell_in = items[i].sell_in - 1;
+
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
               items[i].quality = items[i].quality - 1
-            }
           }
         } else {
           items[i].quality = items[i].quality - items[i].quality

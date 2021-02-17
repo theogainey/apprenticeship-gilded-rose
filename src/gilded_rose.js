9aa4488d5c9a279dfb791ce4482748eab0fb8ff8
Sheridan McKisick <sheridan@heysparkbox.com>
2021-02-17 09:54:54 -0500

    Refactor: Combine nested if statements
    Combined nested if statements where possible (i.e. where there were no nested else statements)
    to refactor and to increase test coverage. Test coverage should now be more than 96%.
    
    FSA2021-33

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -26,23 +26,20 @@ export function updateQuality(items) {
     if (items[i].name === 'Sulfuras, Hand of Ragnaros') {
       break;
     }
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        items[i].quality = items[i].quality - 1
-      }
+    if (items[i].name != 'Aged Brie'
+    && items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+    && items[i].quality > 0) {
+      items[i].quality = items[i].quality - 1
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
+        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
+        && items[i].sell_in < 11
+        && items[i].quality < 50) {
+          items[i].quality = items[i].quality + 1
+          if (items[i].sell_in < 6
+            && items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
           }
         }
       }
@@ -52,10 +49,9 @@ export function updateQuality(items) {
 
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
+        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+        && items[i].quality > 0) {
               items[i].quality = items[i].quality - 1
-          }
         } else {
           items[i].quality = items[i].quality - items[i].quality
         }

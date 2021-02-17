1e0a49e18e6bd8c241d3a6980ffb8bb88799763d
Rise Erpelding <rise@heysparkbox.com>
2021-02-17 11:01:53 -0500

    Chore: Add express server
    Adds hello world page using express, adding start script to use nodemon and babel-node.

---
+++ server.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});

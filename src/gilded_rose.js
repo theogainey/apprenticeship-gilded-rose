501e60a01d016ed9681373f136b4c566ba86311a
Rise Erpelding <rise@heysparkbox.com>
2021-02-22 10:33:02 -0500

    Refactor: extract functions to mutate quality/sellin
    Move all logic that mutates quality/sellin to separate functions so that it can be adjusted later when we want to return a fresh set of data.
    Also handle special rules pertaining to quality limits on items (cannot be over 50) within the extracted functions.
    
    Extract these functions, next plan to consolidate the numbers of times these functions are called.
    This will hopefully make it easier to return a fresh set of data later on.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -21,48 +21,57 @@ const items = [
 
 updateQuality(items);
 */
+
+function mutateItemQuality(currentQuality, amount = 1) {
+  /**
+   * checks if adding the two values would cancel them out and returns 0 before adding
+   * this sets the quality of backstage passes to 0 without needing to check if quality is 50+
+   * otherwise ('Backstage passes to a TAFKAL80ETC concert', -1, 50) is not handled properly
+   */
+  if (-(currentQuality) === amount) {
+    return 0;
+  }
+    // does not allow quality to be increased over 50
+  if (currentQuality >= 50) {
+    return currentQuality;
+  }
+  // quality can increase or decrease, if no amount is specified quality will increase by 1
+  return currentQuality + amount;
+}
+
+function mutateItemSellIn(currentSellIn, amount = 1) {
+  // is generally always decremented (by 1)
+  return currentSellIn - amount;
+}
+
 export function updateQuality(items) {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name === 'Sulfuras, Hand of Ragnaros') {
       break;
     } else if (items[i].name === 'Aged Brie') {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
+        items[i].quality = mutateItemQuality(items[i].quality);
         if (items[i].sell_in < 0) {
-          items[i].quality = items[i].quality + 1
-        }
+          items[i].quality = mutateItemQuality(items[i].quality);
       }
     } else if (items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
+        items[i].quality = mutateItemQuality(items[i].quality);
         if (items[i].sell_in < 11) {
-          items[i].quality = items[i].quality + 1
+        items[i].quality = mutateItemQuality(items[i].quality);
         }
         if (items[i].sell_in < 6) {
-          items[i].quality = items[i].quality + 1
+          items[i].quality = mutateItemQuality(items[i].quality);
         }
-      }
       if (items[i].sell_in <= 0) {
-        items[i].quality = items[i].quality - items[i].quality
+        items[i].quality = mutateItemQuality(items[i].quality, -(items[i].quality));
+
+      }
+    } else {
+      items[i].quality = mutateItemQuality(items[i].quality, -1);
+      if (items[i].sell_in <= 0) {
+        items[i].quality = mutateItemQuality(items[i].quality, -1);
       }
     }
-
-    if (items[i].name != 'Aged Brie'
-    && items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-    && items[i].quality > 0) {
-      // handles updating of general items
-      items[i].quality = items[i].quality - 1
-    } 
-    // handles sell in for all items except sulfuras
-    items[i].sell_in = items[i].sell_in - 1;
-
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-        && items[i].quality > 0) {
-              items[i].quality = items[i].quality - 1
-        } 
-      } 
-    }
+    // handles decrementing of sell in for all items except sulfuras
+    items[i].sell_in = mutateItemSellIn(items[i].sell_in);
   }
 }

7e4a2f76162289f960db68dcfe447346c3b92460
Gamerman2001 <Flash@heysparkbox.com>
2021-02-23 18:44:16 -0500

    refactor: Item.js created and populated The Item class is transfered over to this file Item class is given a constructor with another parameter named HANDLER muTateItemQuality from original gilded_rose.js transferred into this class

--- src/items/Item.js
+++ src/items/Item.js
@@ -1,12 +1,14 @@
+
 class Item {
   constructor(name, sell_in, quality, handler) {
     this.name = name;
     this.sell_in = sell_in
-    this.quality = quality
-    this.handleItem = handler
+    this.quality = quality;
+    this.handleItem = handler;
   }
 
-mutateItemQuality(currentQuality, amount = 1) {
+  // eslint-disable-next-line class-methods-use-this
+  mutateItemQuality(currentQuality, amount = 1) {
     /**
      * checks if adding the two values would cancel them out and returns 0 before adding
      * this sets the quality of backstage passes to 0 without needing to check if quality is 50+
@@ -15,7 +17,7 @@ mutateItemQuality(currentQuality, amount = 1) {
     if (-(currentQuality) === amount) {
       return 0;
     }
-      // does not allow quality to be increased over 50
+    // does not allow quality to be increased over 50
     if (currentQuality >= 50) {
       return currentQuality;
     }
@@ -23,5 +25,22 @@ mutateItemQuality(currentQuality, amount = 1) {
     return currentQuality + amount;
   }
 }
+/*
+* Update inventory
+* @param {Item[]} items - an array of Items representing the inventory to be updated
+* Example usage:
 
-export {Item}
+const items = [
+  new Item('+5 Dexterity Vest', 10, 20, handlerFunction),
+  new Item('Aged Brie', 2, 0, handlerFunction),
+  new Item('Elixir of the Mongoose', 5, 7, handlerFunction),
+  new Item('Sulfuras, Hand of Ragnaros', 0, 80, handlerFunction),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20, handlerFunction),
+  new Item('Conjured Mana Cake', 3, 6, handlerFunction),
+];
+
+updateQuality(items);
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+export { Item };

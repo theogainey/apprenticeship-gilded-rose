bd2f7948dfa8931e07e5fc296b4a15ad0159eaa4
Gamerman2001 <Flash@heysparkbox.com>
2021-02-23 19:49:34 -0500

    refactor: refactor gilded_rose.js - imported Item.js file from src/items/item.js - imported agedBrieHandler from src/items/AgedBrie.js - old mutateItemQuality function still in this file to handle legacy statements for other special items - on line 33 is where we will activate the handleItem method pulled in from the AgedBrie handler

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,26 +1,7 @@
-// Item constructor. DO NOT MODIFY OR THE GOBLIN WILL EAT YOU!
-export function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
-}
+//TARR Made the GOBLIN Eat US! ::sadface::
+const {Item} = require('./items/Item')
+const {agedBrieHandler} = require('./items/AgedBrie')
 
-/*
-* Update inventory
-* @param {Item[]} items - an array of Items representing the inventory to be updated
-* Example usage:
-
-const items = [
-  new Item('+5 Dexterity Vest', 10, 20),
-  new Item('Aged Brie', 2, 0),
-  new Item('Elixir of the Mongoose', 5, 7),
-  new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-  new Item('Conjured Mana Cake', 3, 6),
-];
-
-updateQuality(items);
-*/
 
 function mutateItemQuality(currentQuality, amount = 1) {
   /**
@@ -49,11 +30,7 @@ export function updateQuality(items) {
     if (items[i].name === 'Sulfuras, Hand of Ragnaros') {
       break;
     } else if (items[i].name === 'Aged Brie') {
-        if (items[i].sell_in < 0) {
-          items[i].quality = mutateItemQuality(items[i].quality, 2);
-        } else {
-          items[i].quality = mutateItemQuality(items[i].quality);
-        }
+      items[i].handleItem()
     } else if (items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].sell_in <= 0) {
         items[i].quality = mutateItemQuality(items[i].quality, -(items[i].quality));

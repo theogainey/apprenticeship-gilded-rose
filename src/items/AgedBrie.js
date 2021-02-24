001affcc0a05dcb5b40d7c4c2b894b3085e20b2b
Gamerman2001 <Flash@heysparkbox.com>
2021-02-23 19:07:19 -0500

    refactor: AgedBrie.js created - function agedBrieHandler is created - agedBrieHandler ffunctionality was transferred over from gilded_rose.js - uses the mutateItemQuality method located in the Item.js class

---
+++ src/items/AgedBrie.js
@@ -0,0 +1,9 @@
+function agedBrieHandler() {
+  if (this.sell_in < 0) {
+    this.quality = this.mutateItemQuality(this.quality, 2);
+  } else {
+    this.quality = this.mutateItemQuality(this.quality);
+  }
+}
+
+module.exports = {agedBrieHandler}
